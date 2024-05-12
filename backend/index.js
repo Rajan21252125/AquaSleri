7e2b2eab25598374719a9db8d83fa8fe894e9910
Rajan21252125 <99201190+Rajan21252125@users.noreply.github.com>
2024-05-12 12:30:57 +0530

    added frontend url to cors

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173","https://aquasleri.netlify.app"],
+    origin: ["http://localhost:5173","https://frontendaqua-sleri-cqhchymay-rajan21252125s-projects.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,  
     exposedHeaders: ["Set-Cookie"]
