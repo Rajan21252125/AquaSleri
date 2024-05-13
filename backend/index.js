61a60d496181dc2b436dd396f932945adc4c6f95
Rajan21252125 <99201190+Rajan21252125@users.noreply.github.com>
2024-05-13 23:04:54 +0530

    time paas

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://aquasleri.stranger2125.me"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     exposedHeaders: ["Set-Cookie","*"]
