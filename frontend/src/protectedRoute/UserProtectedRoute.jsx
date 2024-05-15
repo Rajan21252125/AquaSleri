08be84d06f15902951a425c3003eeb2496cbd122
Rajan21252125 <99201190+Rajan21252125@users.noreply.github.com>
2024-05-15 10:12:23 +0530

    security to user login

--- frontend/src/protectedRoute/UserProtectedRoute.jsx
+++ frontend/src/protectedRoute/UserProtectedRoute.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable react/prop-types */
-import { useCookies } from "react-cookie";
 import { Navigate } from "react-router-dom";
 
 const UserProtectedRoute = ({ children }) => {
-    const [cookies] = useCookies([]);
-  if (cookies?.token !== undefined) {
+  const user = localStorage.getItem("role")
+  if (user === "user") {
     return children;
   } else {
     return <Navigate to="/login" />;
